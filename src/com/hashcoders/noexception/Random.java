515809a3423425957489e391c75e124e55106b87
Jeremy KREBS <jeremy@my-krebs.eu>
2014-04-05 12:05:22 +0200

    Random Strategy

--- src/com/hashcoders/noexception/Random.java
+++ src/com/hashcoders/noexception/Random.java
@@ -3,7 +3,7 @@ package com.hashcoders.noexception;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Random {
+public class Random implements Strategy {
 	Data data;
 	List<Intersection> solution;
 	int time;
@@ -12,13 +12,13 @@ public class Random {
 		data = d;
 	}
 	
-	public void nextIntersection(Intersection from) {
+	public void nextIntersection(Intersection from, List<Intersection> path) {
 		for (Road next : from.outgoing) {
 			int cost = next.cost;
 			if (cost <= time) {
 				time -= cost;
 				Intersection to = next.to;
-				solution.add(to);
+				path.add(to);
 				from = to;
 				return;
 			}
@@ -26,10 +26,21 @@ public class Random {
 		time = 0;
 	}
 	
-	public void createPath(Intersection start) {
+	public void createPath(Intersection start, List<Intersection> path) {
 		Intersection temp = start;
 		while (time != 0) {
-			nextIntersection(temp);
+			nextIntersection(temp, solution);
 		}
+	}
+
+	public Solution process(Data d) {
+		int numVeh = data.C;
+		Solution s = new Solution(numVeh);
+		for (int i=0 ; i<numVeh; i++) {
+			List<Intersection> path = new ArrayList<Intersection>();
+			createPath(data.startingIntersection,path);
+			s.paths.get(i).intersections = path;
+		}
+		return s;
 	}
 }
