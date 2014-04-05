c8800874d70811abf606d7552d6131b1a4138fec
Alexandre Bury <alexandre.bury@gmail.com>
2014-04-05 11:41:50 +0200

    Fix list creation

--- src/com/hashcoders/noexception/Intersection.java
+++ src/com/hashcoders/noexception/Intersection.java
@@ -1,13 +1,14 @@
 package com.hashcoders.noexception;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Intersection {
 	float latitude,longitude;
 	int i;
 	
-	List<Road> incoming;
-	List<Road> outgoing;
+	List<Road> incoming = new ArrayList<Road>();
+	List<Road> outgoing = new ArrayList<Road>();
 
 	public Intersection(float latitude, float longitude, int i) {
 		this.latitude = latitude;
