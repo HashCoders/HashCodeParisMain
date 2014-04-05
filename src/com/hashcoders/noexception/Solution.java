2dd6a18fd32a272c9ef11b09d94d2ff19827c6af
Alexandre Bury <alexandre.bury@gmail.com>
2014-04-05 11:32:01 +0200

    Add solution structure and output

--- src/com/hashcoders/noexception/Solution.java
+++ src/com/hashcoders/noexception/Solution.java
@@ -3,11 +3,29 @@ package com.hashcoders.noexception;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class Solution {
+	
+	public static class Path {
+		List<Intersection> intersections;
+	}
+	
+	int C;
+	List<Path> paths;
+	
+	
 	public void toFile(String filename) throws FileNotFoundException, UnsupportedEncodingException {
 		PrintWriter writer = new PrintWriter(filename, "UTF-8");
 
+		writer.println(C);
+		for (int i = 0; i < C; i++)
+		{
+			writer.println(paths.get(i).intersections.size());
+			for (int j = 0; j < paths.get(i).intersections.size(); j++)
+				writer.println(paths.get(i).intersections.get(j).i);
+		}
+		
 		writer.close();
 	}
 }
